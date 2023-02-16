8796306c1f65fef41955c43dd8dddfaddce83756
Cody Grogan <A02313514@usu.edu>
2023-02-15 19:22:43 -0700

    Finished Assignment

--- Assn3/HWK3_Grogan_Cody.py
+++ Assn3/HWK3_Grogan_Cody.py
@@ -3,11 +3,17 @@ import matplotlib.pyplot as plt
 import os
 import pandas as pd
 import seaborn as sns
-from fitter import Fitter, get_common_distributions
+from fitter import Fitter
 from scipy import stats
 
 def callPayoff(strike, current):
     return max(0, current - strike)
+
+def callPayoffAve(target, price1, price2):
+    return max(0, target - (price1 + price2)/2)
+
+def callPayoffMax(target, price1, price2):
+    return max(0, target - max(price1, price2))
 
 class BrownianSimulation:
     def __init__(self, mu: float, vol: float, S_0: float, dt: float, T: float, random ):
@@ -41,9 +47,21 @@ def plotArrays(figTitle, saveFile, y_title, x, arrays, x_title = "Time steps"):
     ax.set_title(figTitle)
     ax.set_xlabel(x_title)
     ax.set_ylabel(y_title)
+    
     # Plotting data
     for array in arrays:
         ax.plot(x, array)
+    
+    aves = arrays[0]
+    # Plotting Average Line
+    for array in arrays[1:]:
+        for i in range(len(array)):
+            aves[i] += array[i]
+
+    aves = np.divide(aves, len(arrays))
+
+    ax.plot(x,aves)
+        
     fig.savefig(os.path.join("Figures", saveFile))
 
 def callSimulate(obj: BrownianSimulation):
@@ -60,7 +78,6 @@ def problem1():
     
     brownians = [BrownianSimulation(drift, volatility, S_0, dt, T, randoms) for i in range(pathNum)] 
     paths = [b.simulate() for b in brownians]
-    plotArrays("Brownian motion", "Brownian", "Stock Price $", range(365), paths, "Days")
     
     riskFree = 1.01
     payoffs = [callPayoff(S_0, path[-1]/riskFree) for path in paths]
@@ -68,67 +85,119 @@ def problem1():
     callBinPrice = np.average(payoffs) * 100
     print(f"Sell price for 100 European Call Options is : {callBinPrice}")
 
+    plotArrays("Predicted Target Stock Price", "TargetGraph.png", "Stock Price $", range(365), paths, "Days")
 
 
 def problem2():
+    distributions = ['beta', 'lognorm', 'norm']
     dataset1 = pd.read_csv('Data/stock1.csv')
     dataset2 = pd.read_csv('Data/stock2-1.csv')
 
+    #sns.displot(data=dataset1, x='Stock Price', kind="hist", bins=100, aspect=1.5)
+    #sns.displot(data=dataset2, x='Stock Price', kind="hist", bins=100, aspect=1.5)
+
     price1 = dataset1["Stock Price"].values
     price2 = dataset2["Stock Price"].values
-    
-    sns.displot(data=dataset1, x="Stock Price", kind="hist", bins = 100, aspect = 1.5)
-    sns.displot(data=dataset2, x="Stock Price", kind="hist", bins = 100, aspect = 1.5)    
-    
-    f = Fitter(price1, distributions=get_common_distributions())
-    #f.fit(progress=False)
-    #params1 = f.get_best(method = 'sumsquare_error')["lognorm"]
-    #random1 = lambda params=params1 : stats.lognorm.rvs(s=params["s"]) - 1
 
+    # Normalizing data
+    max1 = max(price1)
+    min1 = min(price1)  
+    for i in range(len(price1)):
+        price1[i] = (price1[i] - min1)/(max1 - min1)
+
+    max2 = max(price2)
+    min2 = min(price2)  
+    for i in range(len(price2)):
+        price2[i] = (price2[i] - min2)/(max2 - min2)
+    
+    shift1 = (100 - min1)/(max1 - min1)
+    shift2 = (100 - min2)/(max2 - min2)
+
+    # Fitting first price set
+    f = Fitter(price1, distributions=distributions)
+    f.fit(progress=False)
+    params1 = f.get_best(method = 'sumsquare_error')['lognorm']
+    print("---Stock 1 Fit Info---")
+    print(f.summary())
+    print(f"Best fit Lognormal with params s: {params1['s']:.4f}, location: {params1['loc']}, scale: {params1['scale']}")
+
+    # Plotting distribution with shift line for sanity
+    '''
+    fig, ax = plt.subplots(figsize=(10,7))
+    r = stats.lognorm.rvs(s=params1["s"], loc=params1['loc'], scale=params1['scale'], size=1000)
+    ax.hist(r, density=True, bins='auto', histtype='stepfilled', alpha=0.2)
+    ax.plot([shift1 for i in range(200)], [.01 * i for i in range(200)])
+    '''
+
+
+    # Fitting second price set
+    f = Fitter(price2, distributions=distributions)
+    f.fit(progress=False)
+    params2 = f.get_best(method = 'sumsquare_error')['lognorm']
+
+    print("---Stock 2 Fit Info---")
+    print(f.summary())
+    print(f"Best fit Lognormal with params s: {params2['s']:.4f}, location: {params2['loc']}, scale: {params2['scale']}")
+
+    # Plotting distribution with shift line for sanity
+    '''
+    fig, ax = plt.subplots(figsize=(10,7))
+    r = stats.lognorm.rvs(s=params2["s"], loc=params2['loc'], scale=params2['scale'], size=1000)
+    ax.hist(r, density=True, bins='auto', histtype='stepfilled', alpha=0.2)
+    ax.plot([shift2 for i in range(200)], [.01 * i for i in range(200)])
+    '''
+
+    # Performing simulation
     volatility = 0.5
     drift = 0.01
     dt = 1/365
     T = 1
     S_0 = 100
     pathNum = 5000
-    
-    #brownians1 = [BrownianSimulation(drift, volatility, S_0, dt, T, random1) for i in range(pathNum)]
+    riskFree = 1.01
 
-    #paths1 = [b.simulate() for b in brownians1]
-    
-    #plotArrays("Brownian motion", "Brownian1", "Stock Price $", range(365), paths1, "Days")
+    # Calculating S_0 shift in normalized data
+    shift1 = (S_0 - min1)/(max1 - min1)
+    shift2 = (S_0 - min2)/(max2 - min2)
 
+    # Random lambdas
+    targetRandom = lambda : np.random.beta(14,6) - .65
+    random1 = lambda params=params1, shift=shift1 : stats.lognorm.rvs(s=params["s"], loc=params['loc'], scale=params['scale']) - shift
+    random2 = lambda params=params2, shift=shift2 : stats.lognorm.rvs(s=params["s"], loc=params['loc'], scale=params['scale']) - shift
 
+    targetBrownians = [BrownianSimulation(drift, volatility, S_0, dt, T, targetRandom) for i in range(pathNum)]
+    brownians1 = [BrownianSimulation(drift, volatility, S_0, dt, T, random1) for i in range(pathNum)]
+    brownians2 = [BrownianSimulation(drift, volatility, S_0, dt, T, random2) for i in range(pathNum)]
 
+    targetPaths = [b.simulate() for b in targetBrownians]
+    paths1 = [b.simulate() for b in brownians1]
+    paths2 = [b.simulate() for b in brownians2]
     
-    f = Fitter(price2, distributions=get_common_distributions())
-    #f.fit(progress=False)
-    #params2 = f.get_best(method = 'sumsquare_error')["chi2"]
-    #random2 = lambda params=params2 : stats.chi2.rvs(df=params["df"]) - 1
+    avePayoff = []
+    maxPayoff = []
+    for target, path1, path2 in zip(targetPaths, paths1, paths2):
+        avePayoff.append(callPayoffAve(target[-1]/riskFree, path1[-1], path2[-1]))
+        maxPayoff.append(callPayoffMax(target[-1]/riskFree, path1[-1], path2[-1]))
 
-    volatility = 0.5
-    drift = 0.01
-    dt = 1/365
-    T = 1
-    S_0 = 100
-    pathNum = 5000
-    
-    #brownians2 = [BrownianSimulation(drift, volatility, S_0, dt, T, random2) for i in range(pathNum)]
+    aveBinPrice = np.average(avePayoff) * 100
+    maxBinPrice = np.average(maxPayoff) * 100
 
-    #paths2 = [b.simulate() for b in brownians2]
-    
-    #plotArrays("Brownian motion", "Brownian2", "Stock Price $", range(365), paths2, "Days")
-    
+    print(f'Sell price for European Call Option with average of 2 other stocks: {aveBinPrice:.2f}')
+    print(f'Sell price for European Call Option with max of 2 other stocks: {maxBinPrice:.2f}')
+
+    plotArrays("Predicted 1st Stock Price", "Stock1Graph.png", "Stock Price $", range(365), paths1, "Days")
+    plotArrays("Predicted 2nd Stock Price", "Stock2Graph.png", "Stock Price $", range(365), paths2, "Days")
 
     
     
 
 
 def main():
-    #problem1()
-    problem2()
-    
+    problem1()
     plt.show()
+    problem2()
+    plt.show()
+    
 
 
 if __name__ == "__main__":
--- Assn3/README.md
+++ Assn3/README.md
@@ -1,4 +1,82 @@
+# CS 5060 Homework 3
 
+## Running Code
+    To run this code, all that needs be done is run the HWK3_Grogan_Cody.py file and the following information
+    and graphs will be outputted:
 
+    - Graphs
+        - Stock price path graph with trend (Black line) for target stock
+        - Stock price path graph with trend (Black line) for stock 1
+        - Stock price path graph with trend (Black line) for stock 2
+    
+    - Info
+        - Fit info Stock 1
+        - Fit info Stock 2
+        - Price of 100 Euro Call Options w/ strike of 100
+        - Price of 100 Euro Call Options w/ strike of average of stock 1 and stock 2
+        - Price of 100 Euro Call Options w/ stike of max of stock 1 and stock 2
 
+## Problem 1
 
+For this section I use geometric brownian motion and the provided probability distribution to determine the
+change in the stock for each time step.
+
+![image](Figures/TargetGraph.png)
+
+100 Option Price: $804.39
+
+The graph above shows the result of using geometric brownian motion to model the stock price. As can be seen
+the price consistently increases to about $109 dollars over the course of the year. This is because the
+distribution the stock follows biased toward increasing than decreasing. As expected from the average end
+price the value of 100 call options should be priced at $804 after reducing the average value by the risk
+free percentage.
+
+## Problem 2
+
+For this problem I loaded in the data from the provided csv files and began by normalizing the data. I did
+this because the GBM function needs a value between 0-1 to correctly predict stock paths. I then used the fitter
+library to fit either a beta, lognormal, and normal distribution to the data and chose the best based off of
+sum squared error.
+
+- Stock 1:
+    - Sum Squared Error:
+        - lognormal: 19.05
+        - normal: 19.08
+        - beta: 19.4
+    - Best Fit:
+        - lognorm:
+            - s: 0.0127
+            - location: -15.06
+            - scale: 15.57
+
+- Stock 2:
+    - Sum Squared Error:
+        - lognormal: 26.81
+        - beta: 26.81
+        - normal: 27.27
+    - Best Fit:
+        - lognorm:
+            - s: 0.047
+            - location: -3.33
+            - scale: 3.77
+
+I then normalized the initial price of $100 dollars and shifted the distribution to the left that amount.
+This then produces distributions which produce GBM paths consistent with the stock trends.
+
+![image](Figures/Stock1Graph.png)
+
+The graph of the paths for stock 1 shows the slightly negative trend of the stock which would increase the call
+option price for the target if they were compared directly. However stock 2 shows a significant difference.
+
+![image](Figures/Stock2Graph.png)
+
+The graph of stock 2 shows this stock out prices the target stock and thus would make the call option worthless.
+This is then mirrored in the price of the target option with the average and max of stock 1 and 2.
+
+Option Price Average: $99.28
+
+Option Price Max: $0.00
+
+These results make sense because stock 2 increases more than the target stock over the year time period. As a result
+the option price with the maximum of the two is worthless and the average of the two is only slightly positive. This
+then means there is significantly less risk for a call option that hinges on the stock of multiple other companies because it is likely that one or more will out price the target stock.
